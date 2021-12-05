fc477eb9fdeaedba6b39baee0d64f34ebbe3312a
Brendan Klein <bklein5@emich.edu>
2021-12-05 12:53:12 -0500

    Adds end user communication messages

--- user_code/Reviewscode.js
+++ user_code/Reviewscode.js
@@ -4,31 +4,6 @@ const { devstate }= require('../config.json');
 const fs = require('fs');
 let jsonData = "";
 
-//Code to add a professor rating 
-async function RateProfessor(message, client)
-{
-    var arg = message.content.slice(6).trim();
-    var profname = (arg.substr(0,arg.indexOf(' ')).toLowerCase());
-
-    fs.readFile('./logs/professors/professors.txt', function (err, data) 
-    {
-        if (err) throw err;
-        if (data.includes(profname))
-        {
-            var review = arg.substr(arg.indexOf(' ')+1);
-
-            var file = ('./logs/professors/' + profname.toLowerCase() + '.txt').toString().split("\n");
-            message.channel.send(`Submitting review for mod review!  Thanks for taking the time to submit a review!`);
-            approveReview(message, review, client, file, profname);
-        }
-        else 
-        {
-            message.channel.send("Sorry, that professor does not exist!");
-        }
-    });
-}
-
-
 // Code to submit a request to remove a professor review by review index
 async function removeRating(message, client) {
     // Formatting: !removerating professor_name review_index
@@ -67,13 +42,13 @@ async function removeRating(message, client) {
         }
         // Else, reviewIndex is valid: invoke approveRemoval
         message.channel.send(`Submitting request to remove review: ${reviews[reviewIndex]}`);
-        approveRemoval(reviews[reviewIndex], client, file, profName);
+        approveRemoval(message, reviews[reviewIndex], client, file, profName);
     });
 }
 
 /* Review request to remove a review
 // Provided the message, the review contents, the index of the review, client, file, and profName */
-async function approveRemoval(review, client, file, profname) {
+async function approveRemoval(incommingMessageObject, review, client, file, profname) {
     client.channels.cache.get(`${contentapprovalchannel}`).send(`Request to remove review for ${profname} ---> ${review}`)
         .then(function (message) {
             message.react('👍').then(() => message.react('👎'));
@@ -95,6 +70,7 @@ async function approveRemoval(review, client, file, profname) {
                     {
                         message.channel.send('You have approved the request to remove the review!');
                         console.log('Removing review from '+file+'--->'+review);
+                        incommingMessageObject.channel.send('Review removal for '+file+' Accepted!');
                         fs.readFile(file, function (err, data) {
                             // The review is confirmed to exist in the message again, and the index found from review contents
                             // This is to prevent issues if file was modified after request was placed
@@ -120,6 +96,7 @@ async function approveRemoval(review, client, file, profname) {
                     else 
                     {
                         message.channel.send('You have disapproved the request to remove the review');
+                        incommingMessageObject.channel.send('Review removal for '+file+' Denied!');
                         return;
                     }
                 })
@@ -142,8 +119,32 @@ function parseReviewsToArr(data) {
     return reviews;
 }
 
+//Code to add a professor rating 
+async function RateProfessor(message, client)
+{
+    var arg = message.content.slice(6).trim();
+    var profname = (arg.substr(0,arg.indexOf(' ')).toLowerCase());
+
+    fs.readFile('./logs/professors/professors.txt', function (err, data) 
+    {
+        if (err) throw err;
+        if (data.includes(profname))
+        {
+            var review = arg.substr(arg.indexOf(' ')+1);
+
+            var file = ('./logs/professors/' + profname.toLowerCase() + '.txt').toString().split("\n");
+            message.channel.send(`Submitting review for mod review!  Thanks for taking the time to submit a review!`);
+            approveReview(message, review, client, file, profname);
+        }
+        else 
+        {
+            message.channel.send("Sorry, that professor does not exist!");
+        }
+    });
+}
+
 //Code for approving a new professor review
-async function approveReview(message, review, client, file, profname) 
+async function approveReview(incommingMessageObject, review, client, file, profname) 
 {
     client.channels.cache.get(`${contentapprovalchannel}`).send(`Review for ${profname} ---> `+review)
         .then(function (message) {
@@ -166,6 +167,7 @@ async function approveReview(message, review, client, file, profname)
                     {
                         message.channel.send('You have approved the review!');
                         console.log('Appending review to '+file+'--->'+review); //DO NOT REMOVE THIS CONSOLE LOG!
+                        incommingMessageObject.channel.send('Review for '+file+' Accepted!');
                         fs.appendFile(`${file}`,"\n"+"\n"+JSON.stringify(`${review}`), 'utf8', (err) => {
                             if (err) throw err;
                         });
@@ -174,6 +176,7 @@ async function approveReview(message, review, client, file, profname)
                     {
                         message.channel.send('You have disapproved the review.');
                         file = ('./logs/professors/trashdump.txt').toString().split("\n");
+                        incommingMessageObject.channel.send('Review for '+file+' Denied!');
                         fs.appendFile(`${file}`,"\n"+`Declined review for ${profname} ---> `+JSON.stringify(`${review}`), 'utf8', (err) => {
                             if (err) throw err;
                         });
